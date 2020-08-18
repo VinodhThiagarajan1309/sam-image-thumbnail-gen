5c0ec2f8d678db266c5d4c23000acb4c0fa33d33
Vinodh Thiagarajan <vinodh.thiagarajan@gmail.com>
2020-08-18 16:32:45 -0500

    - Implement SAT - Used the ImageMagik Util '

--- image-conversion/index.js
+++ image-conversion/index.js
@@ -4,7 +4,9 @@ const path = require('path'),
     extractS3Info = require('./extract-s3.info'),
     silentRemove = require('./silent-remove'),
     OUTPUT_BUCKET = process.env.OUTPUT_BUCKET,
-    supportedFormats = ['jpg', 'png', 'jpeg', 'gif'];
+    supportedFormats = ['jpg', 'png', 'jpeg', 'gif'],
+    THUMB_WIDTH = process.env.THUMB_WIDTH,
+    childProcessPromise = require('./child-process-promise');
 
 exports.handler = async (event, context) => {
     const s3Info = extractS3Info(event),
@@ -13,13 +15,15 @@ exports.handler = async (event, context) => {
         tempFile = path.join(os.tmpdir(), id + extension),
         extensionWithoutDot = extension.slice(1),
         contentType = `image/${extensionWithoutDot}`;
-    console.log('Vinodh extensionWithoutDot ', extensionWithoutDot);
+
     console.log('converting', s3Info.bucket, ':', s3Info.key, tempFile);
     if (!supportedFormats.includes(extensionWithoutDot)) {
         throw new Error(`Unsupported file type ${extension}`);
     }
 
     await s3Util.downloadFileFromS3(s3Info.bucket, s3Info.key, tempFile);
+
+    await childProcessPromise.spawn('/opt/bin/mogrify',['-thumbnail',`${THUMB_WIDTH}x`, tempFile],);
     await s3Util.uploadFileToS3(OUTPUT_BUCKET, s3Info.key, tempFile, contentType);
     await silentRemove(tempFile);
 
